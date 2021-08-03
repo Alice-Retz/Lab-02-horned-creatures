f8a5f9c032a56c9406172adf97567f833d849436
Alice-Retz <alicegretz@gmail.com>
2021-08-03 15:50:21 -0500

    Initial file set up, App.js started.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import images from './data.js';
 
 function App() {
-  return (
+  return() {
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Horned Creatures</h1>
+      <h3>(Of a Sort)</h3>
+      <p>Sort by number of horns</p>
+      <select onChange={this.handleChange}>
+        <option value="All">All</option>
+        <option value="One">One</option>
+        <option value="Two">Two</option>
+        <option value="Three">Three</option>
+        <option value="Definitely More Than Three, yikes.">Definitely More Than Three, yikes.</option>
+      </select>
+      <ImageList filteredSearch={filteredData} />
     </div>
-  );
+  }
 }
 
 export default App;
